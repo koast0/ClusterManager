4956fe43ae44a96936011f80600695c7cf2beb8e
Иван Гущенко-Чеверда <ivancheverda@MacBook-Pro-Ivan.local>
2015-10-20 23:22:40 +0300

    m/a communiction done. Working on process managment on agent

--- agent.py
+++ agent.py
@@ -2,7 +2,8 @@ import socket
 import json
 import argparse
 import requests
-
+import time
+import subprocess
 
 class JSONrequest:
 
@@ -16,13 +17,14 @@ class JSONrequest:
 
 def Register(ip, port, name):
     data = JSONrequest(status="READY", name=name)
+    print (data.compile())
     headers = {"Accept": "application/json"}
 #print ('@@@@@@@@@@@@@@@@@@@@@@@',json.loads(request.compile()))
-    print(ip+':'+str(port)+'/')
+    print(ip+':'+str(port))
     server_result = requests.post(ip+':'+str(port)+'/', data=data.compile(),
                     headers = headers)
-    print (server_result)
-    return json.loads(server_result)
+    print (server_result.content)
+    return json.loads(server_result.text.decode("utf-8"))
 
 
 def ParceInput(address, port, name):
@@ -36,6 +38,19 @@ def ParceInput(address, port, name):
     port = parser.parse_args().port
     name = parser.parse_args().name
     return (address, port, name)
+def StartProc(args, working_on):
+    proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=stdout_file, stderr=stderr_file,
+                            universal_newlines=True)
+
+    working_on.append(proc)
+def DoAction(action):
+    if action["action"] = "wait":
+        return 0
+    if action["action"] = "exec":
+        for i in action[task]:
+            StartProc(i)
+            return 0
+
 
 
 def main():
@@ -43,9 +58,10 @@ def main():
     address = ''
     name = ''
     address, port, name = ParceInput(address, port, name)
-    Register(address, port, name)
-
-
+    while True:
+        action = Register(address, port, name)
+        DoAction(action)
+        time.sleep(60)
 
 if __name__ == "__main__":
     main()
--- master.py
+++ master.py
@@ -8,7 +8,6 @@ import tornado.web
 import tornado.ioloop
 import tornado.web
 from datetime import datetime
-from jsonhandler import JsonHandler
 
 
 class Node:
@@ -22,31 +21,39 @@ class Node:
 
 
 class IndexHandler(tornado.web.RequestHandler):
-    def __init__(self, config):
+    def initialize(self, config):
         self.config=config
 
     def post(self):
-        print (121)
+        # print(self.request.body.decode("utf-8"))
         if self.request.body:
             try:
-                json_data = json.loads(self.request.body)
+                json_data = json.loads(self.request.body.decode("utf-8"))
                 self.request.arguments.update(json_data)
             except:
-                pass
+                print("failed with parcing json")
+                return
         self.response = dict()
         try:
             name = self.request.arguments['name']
             status = self.request.arguments['status']
         except:
+            print("Some nessesary json object are not available")
             return
         if (status == "READY"):
             for i in self.config:
-                if (i.hostname == name and i.address ==
-                        self.client_address[0]):
-                    self.response['task'] = i.process
-            self.write_json()
+                if (i.hostname == name && status = 0):
+                    if not ('task' in self.response):
+                        self.response['task'] =  [i.process]
+                    else:
+                        self.response['task'].append(i.process)
+                    self.response['action'] = "exec"
+                    status = 1;
+            if not ('task' in self.response):
+                self.response[action] = "wait"
+
         if (status == "FAIL"):
-            f = open("log.txt", 'w')
+            f = open("log.txt", 'a')
             f.write("Process on " + name + " failed\n")
             f.close()
         if (status == "OK"):
@@ -55,7 +62,7 @@ class IndexHandler(tornado.web.RequestHandler):
                         self.client_address[0]):
                     i.last_check_in = datetime.now().time()
         output = json.dumps(self.response)
-        self.write(output)
+        self.finish(output)
 
 
 
@@ -89,9 +96,12 @@ def StartTornado(port, config):
     server.listen(port)
     tornado.ioloop.IOLoop.instance().start()
 
-
+def LogFile():
+    f = open("log.txt", 'w')
+    f.close()
 def main():
     config = []
+    LogFile()
     GetConfigData(config)
     StartTornado(8000, config)
 
